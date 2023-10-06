e6d7fd19e8153396f323219015e95e7fb9818ea9
Aswathy Devi <aswathydevi80@gmail.com>
2023-10-06 10:31:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import React,{useState} from 'react';
 import './style.css'
 import Counter from './Counter';
+import Remainder from './components/Remainder';
 
 function App() {
   return (
-    <div style={{display:"flex",justifyContent:'center',flexDirection:"column",alignItems:'center'}}>
-     <h1>Counter App</h1>
-    <Counter/>
-    </div>
+    // <div style={{display:"flex",justifyContent:'center',flexDirection:"column",alignItems:'center'}}>
+    //  <h1>Counter App</h1>
+    // <Counter/>
+    // </div>
+
+    <div>
+      <Remainder/>
+      </div>
   )
 }
 
